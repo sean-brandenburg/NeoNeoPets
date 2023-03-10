a7b801676e3c0bc99107cbd8f5c15006615497b1
Wenqin Ye <wenqin908@gmail.com>
2023-03-10 10:41:44 -0500

    save

--- src/routes/fishView.svelte
+++ src/routes/fishView.svelte
@@ -166,7 +166,7 @@
                 </div>
                 <div class="stat-title">Hunger</div>
         
-                <div class="stat-value text-primary"> {($petStats.atx.hunger == -1) ? '...' : `${$petStats.atx.hunger}/ 100`}</div>
+                <div class="stat-value text-primary"> {($petStats[location].hunger == -1) ? '...' : `${$petStats[location].hunger}/ 100`}</div>
                 </div>
         
                 <div class="stat">
@@ -174,7 +174,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                 </div>
                 <div class="stat-title text-lg">Thirst</div>
-                <div class="stat-value text-secondary"> {($petStats.atx.thirst == -1) ? '...' : `${$petStats.atx.thirst}/ 100`}</div>
+                <div class="stat-value text-secondary"> {($petStats[location].thirst == -1) ? '...' : `${$petStats[location].thirst}/ 100`}</div>
                 </div>
         
                 <div class="stat">
@@ -182,7 +182,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                     </div>
                     <div class="stat-title">Cleanliness</div>
-                    <div class="stat-value text-primary"> {($petStats.atx.cleanliness == -1) ? '...' : `${$petStats.atx.cleanliness}/ 100`}</div>
+                    <div class="stat-value text-primary"> {($petStats[location].cleanliness == -1) ? '...' : `${$petStats[location].cleanliness}/ 100`}</div>
                 </div>
                 
                 <div class="stat">
@@ -190,7 +190,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
                     <div class="stat-title">Happiness</div>
-                    <div class="stat-value text-secondary"> {($petStats.atx.happiness == -1) ? '...' : `${$petStats.atx.happiness}/ 100`}</div>
+                    <div class="stat-value text-secondary"> {($petStats[location].happiness == -1) ? '...' : `${$petStats[location].happiness}/ 100`}</div>
                 </div>
             </div>
         </div>
